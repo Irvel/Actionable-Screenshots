a0115611c12dac0ea5e4384fd96ff78f624e4261
Irvel <irvel.nduva@gmail.com>
2017-10-23 03:15:35 -0500

    Unsuccessful attempt to fix iPad layout

--- ActionableScreenshots/ActionableScreenshots/AllScreenshotsViewController.swift
+++ ActionableScreenshots/ActionableScreenshots/AllScreenshotsViewController.swift
@@ -159,7 +159,6 @@ class AllScreenshotsViewController: UIViewController, UICollectionViewDelegate, 
         // cells.  Take the collection view's width, subtract (N-1)*S points for
         // the spaces between the cells, and then divide by N to find the final
         // dimension for the cell's width and height.
-        
         let cellsAcross: CGFloat = 3
         let spaceBetweenCells: CGFloat = 7
         let dim = (collectionView.bounds.width - (cellsAcross - 1) * spaceBetweenCells) / cellsAcross
@@ -173,12 +172,17 @@ class AllScreenshotsViewController: UIViewController, UICollectionViewDelegate, 
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                      for: indexPath) as! CollectionViewCell
         let screenshot = filteredScreenshots[indexPath.row]
         
         // Configure the cell
-        let currentImg = getImage(phAsset: screenshot.image!, width: cellSize.width, height: cellSize.height)
+        let currentImg = getImage(phAsset: screenshot.image!,
+                                  width: cellSize.width,
+                                  height: cellSize.height)
+
         cell.imgView.image = currentImg
+        cell.layer.cornerRadius = 4.0
         
         if indexPath.row < processed {
             cell.activityIndicator.stopAnimating()
@@ -187,21 +191,12 @@ class AllScreenshotsViewController: UIViewController, UICollectionViewDelegate, 
             cell.activityIndicator.startAnimating()
         }
         
-        /*
-        if indexPath.row < nonProcessedScreenshots.count {
-            cell.activityIndicator.startAnimating()
-        }
-        else {
-            cell.activityIndicator.stopAnimating()
-        }*/
-        
-        
         return cell
     }
     
     func getImage(phAsset: PHAsset, width: CGFloat, height: CGFloat) -> UIImage {
         var img: UIImage!
-        PHImageManager.default().requestImage(for: (phAsset), targetSize: CGSize(width: 500, height: 500), contentMode: .aspectFill, options: nil) { (image: UIImage?, info: [AnyHashable: Any]?) -> Void in
+        PHImageManager.default().requestImage(for: (phAsset), targetSize: CGSize(width: phAsset.pixelWidth, height: phAsset.pixelHeight), contentMode: .aspectFill, options: nil) { (image: UIImage?, info: [AnyHashable: Any]?) -> Void in
             img = image
         }
         
