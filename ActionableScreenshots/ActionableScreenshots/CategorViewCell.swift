354c22ca930c5bd02148222e7cb14a4651ba3e61
jorgegil96 <jorgegilcavazos@gmail.com>
2017-11-15 19:42:31 -0600

    Add categories table view and its custom cell

---
+++ ActionableScreenshots/ActionableScreenshots/CategorViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CategorViewCell.swift
+//  ActionableScreenshots
+//
+//  Created by Jorge Gil Cavazos on 11/15/17.
+//  Copyright © 2017 Jesus Galvan. All rights reserved.
+//
+
+import UIKit
+
+class CategorViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbCategory: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
