c802b0111322a8731a78c1d6432bc09f9cf4818e
Irvel <irvel.nduva@gmail.com>
2017-11-22 22:54:19 -0600

    Hotfix for image classification

--- ActionableScreenshots/ActionableScreenshots/Image Classification/ImageClassifier.swift
+++ ActionableScreenshots/ActionableScreenshots/Image Classification/ImageClassifier.swift
@@ -32,11 +32,10 @@ class ImageClassifier {
         - image: The image to be converted
      */
     func toBuffer(from image: UIImage) -> CVPixelBuffer? {
-        let width = Int(image.size.width)
-        let height = Int(image.size.height)
+        let size = 224
         let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
         var pixelBuffer : CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, size, size, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
         guard (status == kCVReturnSuccess) else {
             return nil
         }
@@ -46,13 +45,13 @@ class ImageClassifier {
 
         let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
 
-        let context = CGContext(data: pixelData, width: width, height: width, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        let context = CGContext(data: pixelData, width: size, height: size, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
 
-        context?.translateBy(x: 0, y: CGFloat(width))
+        context?.translateBy(x: CGFloat(size), y: CGFloat(size))
         context?.scaleBy(x: 1.0, y: -1.0)
 
         UIGraphicsPushContext(context!)
-        image.draw(in: CGRect(x: 0, y: 0, width: width, height: width))
+        image.draw(in: CGRect(x: 0, y: 0, width: size, height: size))
         UIGraphicsPopContext()
         CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
 
