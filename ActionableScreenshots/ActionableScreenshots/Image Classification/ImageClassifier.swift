4799a5fc73b67ce777705887cfef865dd9af96e4
Irvel <irvel.nduva@gmail.com>
2017-11-30 07:29:33 -0600

    Generify the function for converting UIImages to PixelBuffers

--- ActionableScreenshots/ActionableScreenshots/Image Classification/ImageClassifier.swift
+++ ActionableScreenshots/ActionableScreenshots/Image Classification/ImageClassifier.swift
@@ -11,51 +11,50 @@ import Photos
 class ImageClassifier {
 
     /**
-     Fetches a 224 X 224 image thumbnail for a given asset as source
+     Fetches an image thumbnail for a given asset as source with the exact specified dimensions
      - Parameters:
         - asset: The storage asset identifier to request the image from
+        - width: The target width for the image to obtain
+        - height: The target height for the image to obtain
      */
-    func fetchSmallImage (from asset: PHAsset?) -> UIImage? {
+    func fetchImage (from asset: PHAsset?, width: Int, height: Int) -> UIImage? {
         let manager = PHImageManager.default()
         let option = PHImageRequestOptions()
         var image: UIImage?
         option.isSynchronous = true
-        manager.requestImage(for: asset!, targetSize: CGSize(width: 224, height: 224), contentMode: .aspectFit, options: option, resultHandler: {(result, info)->Void in
+        option.resizeMode = .exact
+        manager.requestImage(for: asset!, targetSize: CGSize(width: width, height: height), contentMode: .aspectFill, options: option, resultHandler: {(result, info)->Void in
             image = result
         })
         return image
     }
-
+    
     /**
      Converts a UIImage to a CVPixelBuffer
      - Parameters:
-        - image: The image to be converted
+     - image: The image to be converted
      */
-    func toBuffer(from image: UIImage) -> CVPixelBuffer? {
-        let size = 224
-        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-        var pixelBuffer : CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, size, size, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-        guard (status == kCVReturnSuccess) else {
+    func toPixelBuffer (forImage image:CGImage) -> CVPixelBuffer? {
+        let frameSize = CGSize(width: image.width, height: image.height)
+        
+        var pixelBuffer:CVPixelBuffer? = nil
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(frameSize.width), Int(frameSize.height), kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
+        
+        if status != kCVReturnSuccess {
             return nil
         }
-
-        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags.init(rawValue: 0))
+        let data = CVPixelBufferGetBaseAddress(pixelBuffer!)
         let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
+        let context = CGContext(data: data, width: Int(frameSize.width), height: Int(frameSize.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: bitmapInfo.rawValue)
 
-        let context = CGContext(data: pixelData, width: size, height: size, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-
-        context?.translateBy(x: CGFloat(size), y: CGFloat(size))
-        context?.scaleBy(x: 1.0, y: -1.0)
-
-        UIGraphicsPushContext(context!)
-        image.draw(in: CGRect(x: 0, y: 0, width: size, height: size))
-        UIGraphicsPopContext()
+        context?.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
         CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-
+        
         return pixelBuffer
+        
     }
 
     /**
@@ -72,12 +71,14 @@ class ImageClassifier {
     func classify(asset: PHAsset) -> [Tag] {
         let objectsModel = MobileNet()
         let appNameModel = AppCategorizer()
-        let image = fetchSmallImage(from: asset)
-        let pixelBuffer: CVPixelBuffer = toBuffer(from: image!)!
+        let image1 = fetchImage(from: asset, width: 260, height: 424)
+        let image2 = fetchImage(from: asset, width: 224, height: 224)
+        let pixelBuffer1: CVPixelBuffer = toPixelBuffer(forImage: image1!.cgImage!)!
+        let pixelBuffer2: CVPixelBuffer = toPixelBuffer(forImage: image2!.cgImage!)!
         let minProb = 0.65
         var foundTags: [Tag] = []
 
-        if let prediction = try? objectsModel.prediction(image: pixelBuffer) {
+        if let prediction = try? objectsModel.prediction(image: pixelBuffer2) {
             for (label, probability) in (Array(prediction.classLabelProbs).sorted {$0.1 > $1.1}) {
                 if probability >= minProb && !label.lowercased().contains("web site") {
                     var allLabels = label.components(separatedBy: ", ")
@@ -89,7 +90,7 @@ class ImageClassifier {
             }
         }
 
-        if let prediction = try? appNameModel.prediction(image: pixelBuffer) {
+        if let prediction = try? appNameModel.prediction(image: pixelBuffer1) {
             print(Array(prediction.classLabelProbs).description)
             for (label, probability) in (Array(prediction.classLabelProbs).sorted {$0.1 > $1.1}) {
                 if probability >= minProb {
